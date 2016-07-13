06038266d875fb08900751c2414ac9f2fa1f6b13
Devin "GIT SMASH!" Jackson <jackson.t.devin@gmail.com>
2016-07-13 16:22:04 -0400

    removed print statements

--- articles/views.py
+++ articles/views.py
@@ -22,7 +22,6 @@ class ArticleView(View):
     template_name = 'article.html'
 
     def get(self, request, **kwargs):
-        print kwargs
         article = Article.objects.get(slug=kwargs['slug'])
         return render(request, self.template_name, {'article':article})
 
@@ -34,12 +33,7 @@ class SubscribeView(View):
         return render(request, self.template_name)
 
     def post(self, request):
-        print request.POST
-        try:
-            Subscription.objects.create(email=str(request.POST['email']))
-        except Exception as e:
-            print "EXC", e
-            return JsonResponse({'status':500})
+        Subscription.objects.create(email=str(request.POST['email']))
         return JsonResponse({'status':200})
 
 class ResourcesView(View):
